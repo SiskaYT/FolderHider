a014deac2d5f4358c33d834f31dd6bba3181524d
SiskaYT <sichalexandr424@gmail.com>
2024-11-13 21:04:52 +0100

    bl0cag€.py

--- bl0cag€.py
+++ bl0cag€.py
@@ -24,7 +24,7 @@ def unbl0ck_access_t0_f0lders(f0lder_path):
 def restart_expl0rer():
     subprocess.Popen("explorer.exe")
 
-f0lder_path = "D:/!!duležite!!/Ahoj (nevíš nic)" #Here change the folder what do you want hide if someone will send wrong code
+f0lder_path = "C:/your/folder" #Here change the folder what do you want hide if someone will send wrong code
 
 bl0ck_access_t0_f0lders(f0lder_path)
 cl0se_all_expl0rer_windows()

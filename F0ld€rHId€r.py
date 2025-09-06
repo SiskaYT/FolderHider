20615994a79dbb5841be63e4d4928b2f5518a98d
SiskaYT <sichalexandr424@gmail.com>
2025-09-06 17:50:55 +0200

    Update F0ld€rHId€r.py

--- F0ld€rHId€r.py
+++ F0ld€rHId€r.py
@@ -1,5 +1,5 @@
 #!Do NOT copy my code!
-#!If there is error or issue CONTACT owner! (by discord: ten_siska) or (github: https://github.com/SiskaYT)
+#!If there is error or issue CONTACT owner! (by discord: _the_pine_cone_boy_) or (github: https://github.com/SiskaYT)
 #!Do NOT rewrite code under this text it may cause problems (!exept that code where you need to change someting!)!
 
 import os
@@ -7,6 +7,8 @@ import random
 import time
 
 hezl0 = random.randint(10000000, 100000000000000)
+place = "C:/Program Files" #Here set place where is your folder saved.
+folder = "C:/Program Files/Google Backup" #Folder what do you want open after adding correct code (c0d€.dll) .
 
 time.sleep(5)
 
@@ -16,15 +18,16 @@ with open("c0d€.dll", "w", encoding="utf-8") as file:
 time.sleep(10)
 
 with open("c0d€.dll", "w", encoding="utf-8") as file:
-    file.write("")
+    file.write("What are you doing here??!!")
 
 try:
     dáváj = int(input("Davaj:\n"))
     if hezl0 == dáváj:
-        os.chdir("C:/your/place") #Here set place where is your folder saved.
-        os.startfile("C:/your/folder") #Folder what do you want open after adding correct code (c0d€.dll) .
+        os.chdir(place) 
+        os.startfile(folder)
     else:
         print("Wrong answer.")
         os.system("python bl0cag€.py")
+        os.system(f'attrib +h "{folder}"') #This will hide the folder if someone would say wrong password.
 except SyntaxError:
-    print("Something is wrong.")
+    print("Something went wrong.")
